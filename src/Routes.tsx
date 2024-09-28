10b953ea46389cd4a5c2bfbdb534c167e5def36e
unknown <gnferreira2000@gmail.com>
2024-09-28 11:26:34 -0300

    commit

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -7,7 +7,6 @@ import WelcomePage from './components/WelcomePage';
 const AppRoutes: React.FC<{ isLogged: boolean; onLogin: () => void }> = ({ isLogged, onLogin }) => {
   return (
     <Routes>
-      <Route path="*" element={<Navigate to="/brasfoot-futnews" />} />
       <Route
         path="/brasfoot-futnews"
         element={isLogged ? <Navigate to="/brasfoot-futnews/welcome" /> : <LoginPage onLogin={onLogin} />}
