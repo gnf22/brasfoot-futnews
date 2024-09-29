61d9e2c6f54ed62e336a6638fc564d4e7c8edeac
unknown <gnferreira2000@gmail.com>
2024-09-29 19:51:24 -0300

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "brasfoot-futnews/",
+  base: "/brasfoot-futnews/",
+  define: {
+    'process.env': process.env
+  },
   server: {
     open: true,
     port: 3000,
